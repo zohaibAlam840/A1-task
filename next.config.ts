89deda111edbfc0aaf5b146572dc3c312bea6a8b
Zohaib Alam Pervaiz <fiveer840@gmail.com>
2025-12-03 09:47:48 +0500

    error fix falack

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["fakestoreapi.com"],
+    domains: ["fakestoreapi.com",'dummyjson.com'],
   },
 };
 
